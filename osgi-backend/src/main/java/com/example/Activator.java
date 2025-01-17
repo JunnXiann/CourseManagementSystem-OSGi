b9ab5b79c7e9d1f799485ee6acf81e1679bfb308
junxian <junxiansean8@gmail.com>
2025-01-17 17:16:35 +0800

    initial setup

---
+++ osgi-backend/src/main/java/com/example/Activator.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Activator implements org.osgi.framework.BundleActivator {
+
+    @Override
+    public void start(org.osgi.framework.BundleContext context) throws Exception {
+        // Code to execute when the bundle is started
+    }
+
+    @Override
+    public void stop(org.osgi.framework.BundleContext context) throws Exception {
+        // Code to execute when the bundle is stopped
+    }
+}
